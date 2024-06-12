e57aa7dff1a0373753ef83d2e5671b5725ac83f4
ezboy4life <82066433+ezboy4life@users.noreply.github.com>
2024-06-12 17:08:11 -0300

    Finished file_to_data.dart design
    Terminado o design gráfico da página file_to_data.dart.

--- lib/screens/chat_interaction/file_to_data.dart
+++ lib/screens/chat_interaction/file_to_data.dart
@@ -9,6 +9,8 @@ import 'package:app_atex_gpt_exam/services/database.dart';
 import 'package:app_atex_gpt_exam/shared/isolate_manager.dart';
 import 'package:app_atex_gpt_exam/shared/utilities.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:media_store_plus/media_store_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:app_atex_gpt_exam/screens/widgets/input_field.dart';
@@ -52,27 +54,25 @@ class _UploadExamState extends State<UploadExam> {
     }
   }
 
-  Future<bool?> showConfirmationDialog(BuildContext context) async {
+  Future<bool?> showConfirmationDialog(BuildContext context) {
     return showDialog<bool>(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Confirm Sign Out'),
-          content: const Text('Are you sure you want to sign out?'),
+          title: const Text('Deseja sair de sua conta?'),
+          content: const Text('Você precisará fazer Login novamente.'),
           actions: <Widget>[
             TextButton(
               onPressed: () {
-                Navigator.of(context)
-                    .pop(false); // Return false when No is pressed
+                Navigator.of(context).pop(false);
               },
-              child: const Text('No'),
+              child: const Text('Não'),
             ),
             TextButton(
               onPressed: () {
-                Navigator.of(context)
-                    .pop(true); // Return true when Yes is pressed
+                Navigator.of(context).pop(true);
               },
-              child: const Text('Yes'),
+              child: const Text('Sim'),
             ),
           ],
         );
@@ -110,9 +110,11 @@ class _UploadExamState extends State<UploadExam> {
               ),
             ),
             IconButton(
-              onPressed: () {
+              onPressed: () async {
                 bool? shouldSignOut = await showConfirmationDialog(context);
-                AuthService().signOut();
+                if (shouldSignOut == true) {
+                  AuthService().signOut();
+                }
               },
               icon: const Icon(
                 Icons.exit_to_app,
@@ -125,47 +127,46 @@ class _UploadExamState extends State<UploadExam> {
       ),
       backgroundColor: Colors.black,
       body: Center(
-        child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(40),
-                  child: SizedBox(
-                      height: 400,
-                      child: UploadStepper(
-                          userUID: widget.userUID, csvRecord: csvRecord)),
-                ),
-                Container(
-                  width: double.infinity,
-                  decoration: ShapeDecoration(
-                    shape: const StadiumBorder(),
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.blue.shade600,
-                        Colors.blue.shade900,
-                      ],
-                    ),
-                  ),
-                  child: TextButton(
-                    style: const ButtonStyle(
-                      splashFactory: NoSplash.splashFactory,
-                    ),
-                    onPressed: () {
-                      acquireCsvList();
-                    },
-                    child: const Text(
-                      "Escolher arquivo",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child:
+                  UploadStepper(userUID: widget.userUID, csvRecord: csvRecord),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40.0, 20, 40.0, 0),
+              child: Container(
+                width: double.infinity,
+                decoration: ShapeDecoration(
+                  shape: const StadiumBorder(),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.blue.shade600,
+                      Colors.blue.shade900,
+                    ],
                   ),
                 ),
-              ],
-            )),
+                child: TextButton(
+                  style: const ButtonStyle(
+                    splashFactory: NoSplash.splashFactory,
+                  ),
+                  onPressed: () {
+                    acquireCsvList();
+                  },
+                  child: const Text(
+                    "Escolher arquivo",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -210,18 +211,14 @@ class _UploadStepperState extends State<UploadStepper> {
     await Future.wait(futures);
 
     showDialog(
-        context: context,
-        builder: (context) => const AlertDialog(
-              icon: Icon(Icons.warning),
-              iconColor: Colors.blueAccent,
-              title: Text("Em tese, seu arquivo foi salvo!"),
-              content: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("\nNa próxima dev version, conversaremos com o GPT!"),
-                ],
-              ),
-            ));
+      context: context,
+      builder: (context) => const AlertDialog(
+        icon: Icon(Icons.warning),
+        iconColor: Colors.blueAccent,
+        title: Text("Seu arquivo foi salvo!"),
+        content: Text("Na próxima versão, conversaremos com o GPT!"),
+      ),
+    );
 
     await IsolateManager().initializeIsolate();
     for (Question q in widget.csvRecord!.questionAggregator.questions) {
@@ -244,86 +241,144 @@ class _UploadStepperState extends State<UploadStepper> {
   @override
   Widget build(BuildContext context) {
     if (widget.csvRecord != null) {
-      return Stepper(
-        currentStep: _currentStep,
-        onStepTapped: (int newIndex) {
-          setState(() {
-            _currentStep = newIndex;
-          });
-        },
-        onStepContinue: () {
-          if (_currentStep < 1) {
+      return Container(
+        height: 450,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white24),
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          color: Colors.black,
+        ),
+        child: Stepper(
+          currentStep: _currentStep,
+          connectorColor: MaterialStateProperty.resolveWith<Color>((states) {
+            if (states.contains(MaterialState.selected)) {
+              return Colors.blue;
+            } else {
+              return Colors.grey;
+            }
+          }),
+          onStepTapped: (int newIndex) {
             setState(() {
-              _currentStep += 1;
+              _currentStep = newIndex;
             });
-          } else {
-            // esse else significa que acabou os steps, agora é enviar essa bomba pro banco de dados e pro gpt
-            submitData();
-          }
-        },
-        onStepCancel: () {
-          if (_currentStep > 0) {
-            setState(() {
-              _currentStep -= 1;
-            });
-          }
-        },
-        type: StepperType.horizontal,
-        steps: [
-          Step(
-            title: const Text("Exam Info"),
-            isActive: _currentStep == 0,
-            content: Column(
-              children: [
-                InputField(
-                  controller: examName,
-                  labelText: "Nome da Avaliação",
-                  obscureText: false,
-                  validator: null,
-                  onChanged: null,
-                ),
-                const SizedBox(height: 20),
-                InputField(
-                  controller: examDate,
-                  labelText: "Data da Avaliação",
-                  obscureText: false,
-                  validator: null,
-                  onChanged: null,
-                ),
-                const SizedBox(height: 20),
-                InputField(
-                  controller: examCourse,
-                  labelText: "Curso",
-                  obscureText: false,
-                  validator: null,
-                  onChanged: null,
-                ),
-                const SizedBox(height: 20),
-                InputField(
-                  controller: examSubject,
-                  labelText: "Assunto",
-                  obscureText: false,
-                  validator: null,
-                  onChanged: null,
-                ),
-              ],
+          },
+          onStepContinue: () {
+            if (_currentStep < 1) {
+              setState(() {
+                _currentStep += 1;
+              });
+            } else {
+              // esse else significa que acabou os steps, agora é enviar essa bomba pro banco de dados e pro gpt
+              submitData();
+            }
+          },
+          onStepCancel: () {
+            if (_currentStep > 0) {
+              setState(() {
+                _currentStep -= 1;
+              });
+            }
+          },
+          type: StepperType.horizontal,
+          controlsBuilder: (BuildContext context, ControlsDetails details) {
+            return Container(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextButton(
+                      onPressed: details.onStepCancel,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red[700],
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.arrow_back),
+                          Text('Cancelar'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: details.onStepContinue,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue[700],
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Avançar'),
+                          Icon(Icons.arrow_forward),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          steps: [
+            Step(
+              title: const Text("Info."),
+              isActive: _currentStep == 0,
+              content: Column(
+                children: [
+                  InputField(
+                    controller: examName,
+                    labelText: "Nome da Avaliação",
+                    obscureText: false,
+                    validator: null,
+                    onChanged: null,
+                  ),
+                  const SizedBox(height: 20),
+                  InputField(
+                    controller: examDate,
+                    labelText: "Data da Avaliação",
+                    obscureText: false,
+                    validator: null,
+                    onChanged: null,
+                  ),
+                  const SizedBox(height: 20),
+                  InputField(
+                    controller: examCourse,
+                    labelText: "Curso",
+                    obscureText: false,
+                    validator: null,
+                    onChanged: null,
+                  ),
+                  const SizedBox(height: 20),
+                  InputField(
+                    controller: examSubject,
+                    labelText: "Assunto",
+                    obscureText: false,
+                    validator: null,
+                    onChanged: null,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Step(
-            isActive: _currentStep == 1,
-            title: const Text("Questions and Answers"),
-            content: Column(
-              children: [
-                for (Question q
-                    in widget.csvRecord!.questionAggregator.questions)
-                  QuestionDataWidgetCompact(
+            Step(
+              isActive: _currentStep == 1,
+              title: const Text("Respostas"),
+              content: Column(
+                children: [
+                  for (Question q
+                      in widget.csvRecord!.questionAggregator.questions)
+                    QuestionDataWidgetCompact(
                       question: q,
                       aa: widget.csvRecord!.answerAggregatorList
-                          .firstWhere((aa) => aa.uid == q.uid)),
-              ],
+                          .firstWhere((aa) => aa.uid == q.uid),
+                    ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     } else {
       return const Placeholder();
@@ -342,16 +397,24 @@ class QuestionDataWidgetCompact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("${question.questionBody}"),
+        Text(
+          "${question.questionBody}",
+          style: TextStyle(color: Colors.blue.shade700),
+        ),
+        const SizedBox(height: 10),
         Container(
           padding: const EdgeInsets.only(left: 10),
           child: Column(
             children: [
               for (Answer a in aa.answers)
-                Text("Student: ${a.studentUID} \n${a.studentAnswer}"),
+                Text(
+                  "Student: ${a.studentUID} \n${a.studentAnswer}",
+                  style: const TextStyle(color: Colors.white),
+                ),
             ],
           ),
         ),
+        const SizedBox(height: 20),
       ],
     );
   }
